a0a257de667f1e670d59698758ec053891cab11e
Lucas <lucasrafael123araujolima@gmail.com>
2022-11-22 04:43:55 -0400

    feat: :sparkles: Organization

--- src/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express")
-
-const app = express()
-
-app.get("/", (request, response) => response.status(200).send("Hello World!"))
-
-app.listen(3333, () => console.log("Server Running or port 3333"))
